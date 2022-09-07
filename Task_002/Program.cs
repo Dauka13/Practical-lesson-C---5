382ff00f986364bdcb8a06883dd1b8b07365c880
Dauka <orazbekovdd13@mail.ru>
2022-09-07 18:42:32 +0600

    add task 002 about two-dimensiol array a==m+n

---
+++ Task_002/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int InputNumber(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int[,] FillMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+     for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int rows = InputNumber("Enter number of lines: ");
+int columns = InputNumber("Enter number of columns: ");
+
+int[,] matrix = FillMatrix(rows, columns);
+
+Console.WriteLine("Two-dimensiol array: ");
+PrintMatrix(matrix);
